1a3a32cdc2acb353263f88420beccd86e0f17cb2
Affan Suhendar <affansuhendar1@gmail.com>
2025-07-25 01:21:44 +0700

    update

--- Inforgather/Mencari IP/CheckIP.py
+++ Inforgather/Mencari IP/CheckIP.py
@@ -3,10 +3,20 @@ import requests
 import pprint
 import json
 
-hostname = input("Enter hostname: ")
+def Intro(Judul):
+    print(("+")+("-"*(len(Judul)+2))+("+"))
+    print(f"| {Judul} |")
+    print(("+")+("-"*(len(Judul)+2))+("+"))
+
+Intro("Check IP")
+
+print("Contoh Hostname : google.com")
+hostname = input("Input Hostname  : ")
 ip_address = socket.gethostbyname(hostname)
 
 url = "https://geolocation-db.com/jsonp/" + ip_address
+print("\n")
+print(f"Info Hostname dari {hostname}")
 response = requests.get(url)
 geolocation = response.content.decode()
 geolocation = geolocation.split("(")[1].strip(")")
@@ -15,3 +25,4 @@ geolocation = json.loads(geolocation)
 for k,v in geolocation.items():
     pprint.pprint(f"{k}: {v}")
 
+print("\n")
